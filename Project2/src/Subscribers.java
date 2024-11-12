8fe401320ff94ad0a73bc5a9190a08bfee6587aa
Reemas Alyahia <130400514+Reemas-Alyahia@users.noreply.github.com>
2024-11-12 11:55:32 +0300

    Update Subscribers.java

--- Project2/src/Subscribers.java
+++ Project2/src/Subscribers.java
@@ -6,6 +6,7 @@ public class Subscribers extends Passenger {
     @Override
     public void reservedCar(Car c) throws Exception {
     this.reservedCar=c;
+     c.setMaximumcap(c.getMaximumcap()-1);
     this.tripCost=c.getRoute().getTripPrice()-(c.getRoute().getTripPrice()*0.5);
 c.resverC();
     }
